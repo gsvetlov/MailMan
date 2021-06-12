1aa376614cb3962f71549ac11d7ad3bcddb779dd
Georg Svetlov <gsvetlov@gmail.com>
2021-06-12 21:41:55 +0300

    добавлен static класс с тестовыми настройками почтового клиента, отправка почты вынесена в отдельный сервис

---
+++ MailMan/Services/MailSenderService/MessageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using MailMan.Services.MailSenderService.Enums;
+
+namespace MailMan.Services.MailSenderService
+{
+    public class MessageConfiguration
+    {
+        public MessagePriority Priority { get; set; }
+        public string From { get; set; }
+        public List<string> To { get; set; }
+        public List<string> CC { get; set; }
+        public List<string> ReplyTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
+
+    }
+}
---
+++ MailMan/Services/MailSenderService/SendMailResult.cs
@@ -0,0 +1,15 @@
+﻿namespace MailMan.Services.MailSenderService
+{
+    public record SendMailResult
+    {
+        public SendMailResult() { }
+        public SendMailResult(bool isSuccessfull, string message)
+        {
+            IsSuccessfull = isSuccessfull;
+            Message = message;
+        }
+
+        public bool IsSuccessfull { get; init; }
+        public string Message { get; init; }
+    }
+}
