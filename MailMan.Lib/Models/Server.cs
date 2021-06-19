4990853b62ac2a5245a6b0e7f00750c983c20fea
Georg Svetlov <gsvetlov@gmail.com>
2021-06-19 18:06:29 +0300

    диалог редактирования/добавления адресов отправителей, добавление/редактирование/удаление записей

--- MailMan.Lib/Models/Server.cs
+++ MailMan.Lib/Models/Server.cs
@@ -2,6 +2,7 @@
 {
     public class Server
     {
+        public int Id { get; set; }
         public string Name { get; set; }
 
         public string Address { get; set; }
@@ -14,6 +15,6 @@
 
         public string Password { get; set; }
 
-        public override string ToString() => $"{Name}:{Port}";
+        public string Description { get; set; }
     }
 }
--- MailMan/Views/MainWindow/MainWindow.xaml.cs
+++ MailMan/Views/MainWindow/MainWindow.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Windows;
 using System.Windows.Media;
 
+using MailMan.Data;
 using MailMan.Models;
 using MailMan.Services.MailSenderService;
 using MailMan.ViewModels;
+using MailMan.Views.UserDialog;
 using MailMan.Views.UserNotificationDialog;
 
 namespace MailMan
@@ -40,14 +43,12 @@ namespace MailMan
             if (ctx.SenderListSelected is not Sender sender) return;
             if (RecipientsList.SelectedItem is not Recipient recipient) return;
             if (ctx.ServerListSelected is not Server server) return;
-            if (MessagesList.SelectedItem is not Message message) return;
+            if (cbMesagesList.SelectedItem is not Message message) return;
 
-            var mail_sender = ctx.MailSender;
-            mail_sender.SendMailResultEvent += DisplayUserNotification;
 
             var serverConfig = new ServerConfiguration
             {
-                Host = server.Name,
+                Host = server.Address,
                 Port = server.Port,
                 UseSSL = server.UseSSL,
                 Credentials = new NetworkCredential(server.Login, server.Password),
@@ -61,15 +62,15 @@ namespace MailMan
                 Body = message.Text
             };
 
-            try
+            ctx.MailSender.SendMailResultEvent += DisplayUserNotification;
+            ctx.MailSender.SendMail(messageConfig, serverConfig);
+
+            /*try
             {
-                // Запускаем таймер
-                var timer = Stopwatch.StartNew();
-                // И запускаем процесс отправки почты
+                mail_sender.SendMailResultEvent += DisplayUserNotification;
                 mail_sender.SendMail(messageConfig, serverConfig);
-                timer.Stop();
                 MessageBox.Show(
-                $"Почта успешно отправлена за {timer.Elapsed.TotalSeconds:0.##}c",
+                $"Почта успешно отправлена",
                 "Отправка почты",
                 MessageBoxButton.OK,
                 MessageBoxImage.Information);
@@ -86,7 +87,7 @@ namespace MailMan
             finally
             {
                 mail_sender.SendMailResultEvent -= DisplayUserNotification;
-            }
+            }*/
         }
 
         private void DisplayUserNotification(object sender, SendMailResult result)
@@ -97,17 +98,131 @@ namespace MailMan
             ctx.Title = "Отправка почты";
             if (result.IsSuccessfull)
             {
-                
+
                 ctx.NotificationText = "Письмо было успешно отправлено";
                 ctx.TextForeground = Brushes.DarkGreen;
             }
             else
             {
                 ctx.NotificationText = $"Письмо не было отправлено по причине:{Environment.NewLine}{result.Message}";
-                ctx.TextForeground = Brushes.DarkRed ;
-            }          
-            
+                ctx.TextForeground = Brushes.DarkRed;
+            }
+
             uDialog.ShowDialog();
         }
+
+        private void OnAddServerButtonClick(object _sender, RoutedEventArgs E)
+        {
+            if (!ServerEditDialog.Create(
+            out var name,
+            out var address,
+            out var port,
+            out var ssl,
+            out var description,
+            out var login,
+            out var password))
+                return;
+            var server = new Server
+            {
+                Id = TestData.Servers.DefaultIfEmpty().Max(s => s.Id) + 1,
+                Name = name,
+                Address = address,
+                Port = port,
+                UseSSL = ssl,
+                Description = description,
+                Login = login,
+                Password = password
+            };
+
+            var ctx = (MainWindowViewModel)DataContext;
+            ctx.Servers.Add(server);
+            ServersList.SelectedItem = server;
+        }
+
+        private void OnEditServerButtonClick(object _sender, RoutedEventArgs e)
+        {
+            Server server = (Server)ServersList.SelectedItem;
+            var ctx = (MainWindowViewModel)DataContext;
+            int idx = ctx.Servers.IndexOf(server);
+            var name = server.Name;
+            var address = server.Address;
+            var port = server.Port;
+            var ssl = server.UseSSL;
+            var descr = server.Description;
+            var login = server.Login;
+            var pwd = server.Password;
+
+            if (!ServerEditDialog.ShowDialog(
+                "Редактирование сервера",
+                ref name,
+                ref address,
+                ref port,
+                ref ssl,
+                ref descr,
+                ref login,
+                ref pwd)) return;
+
+            var updServer = ctx.Servers[idx];
+            updServer.Name = name;
+            updServer.Address = address;
+            updServer.Port = port;
+            updServer.UseSSL = ssl;
+            updServer.Description = descr;
+            updServer.Login = login;
+            updServer.Password = pwd;
+            ServersList.SelectedItem = ctx.Servers[idx];
+        }
+
+        private void OnRemoveServerButtonClick(object _sender, RoutedEventArgs e)
+        {
+            var ctx = (MainWindowViewModel)DataContext;
+            ctx.Servers.Remove(ctx.ServerListSelected);
+            ServersList.SelectedIndex = 0;
+        }
+
+        private void OnAddSenderButtonClick(object _sender, RoutedEventArgs e)
+        {
+            if (!SenderEditDialog.Create(
+                "Добавить отправителя",
+                out string name,
+                out string address,
+                out string descriprion)) return;
+            var sender = new Sender
+            {
+                Id = TestData.Senders.DefaultIfEmpty().Max(s => s.Id) + 1,
+                Name = name,
+                Address = address,
+                Description = descriprion
+            };
+            var ctx = (MainWindowViewModel)DataContext;
+            ctx.Senders.Add(sender);
+            SendersList.SelectedItem = sender;
+        }
+
+        private void OnEditSenderButtonClick(object _sender, RoutedEventArgs e)
+        {
+            var ctx = (MainWindowViewModel)DataContext;
+            var idx = ctx.Senders.IndexOf(ctx.SenderListSelected);
+            string name = ctx.SenderListSelected.Name;
+            string address = ctx.SenderListSelected.Address;
+            string description = ctx.SenderListSelected.Description;
+            if (!SenderEditDialog.ShowDialog(
+                "Редактировать отправителя",
+                ref name,
+                ref address,
+                ref description)) return;
+            ctx.Senders[idx].Name = name;
+            ctx.Senders[idx].Address = address;
+            ctx.Senders[idx].Description = description;
+            SendersList.SelectedItem = ctx.Senders[idx];
+        }
+
+        private void OnDeleteSenderButtonClick(object _sender, RoutedEventArgs e)
+        {
+            var ctx = (MainWindowViewModel)DataContext;
+            ctx.Senders.Remove(ctx.SenderListSelected);
+            ServersList.SelectedIndex = 0;
+        }
+
     }
 }
